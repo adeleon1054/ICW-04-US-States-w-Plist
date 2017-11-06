3bf75f122b081ae9fe4cafcb2abd9b490b5080ba
user <user@W347439.local>
2017-11-06 18:59:39 -0500

    Initial Commit

---
+++ sc00_PlistAndTableViews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sc00_PlistAndTableViews
+//
+//  Created by user on 11/6/17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sc00_PlistAndTableViews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sc00_PlistAndTableViews
+//
+//  Created by user on 11/6/17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
